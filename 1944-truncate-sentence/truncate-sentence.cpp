d078a953e2e7255d64f32df199e72d55dbccfd4c
Piyush Anand <contact.piyushanand@gmail.com>
2025-11-02 14:22:23 +0530

    Time: 2 ms (12.54%) | Memory: 11 MB (5.14%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count=0;
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(count<k){
+                if(s[i]==' '){
+                    count++;
+                    if(count==k){
+                        break;
+                    }
+                }
+                ans=ans+s[i];
+            }
+        }
+        return ans;
+    }
+};
