5a4035099906198564a34de31c2dd2cc2380aa6c
Piyush Anand <contact.piyushanand@gmail.com>
2025-11-11 10:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (8.77%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int operation=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<k){
+                operation++;
+            }
+        }
+        return operation;
+        
+    }
+};
