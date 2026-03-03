6ff97f53b58ca658d9707eb495ba01cabd539e56
Piyush Anand <contact.piyushanand@gmail.com>
2026-03-03 14:57:38 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (49.73%) - LeetSync

---
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr != NULL){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    void insertAtTail(ListNode*& head, ListNode*& tail, int data){
+        ListNode* temp = new ListNode(data);
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+            return;
+        }
+        tail->next = temp;
+        tail = temp;
+    }
+
+    ListNode* add(ListNode* l1, ListNode* l2){
+        int carry = 0;
+        ListNode* ansHead = NULL;
+        ListNode* ansTail = NULL;
+
+        while(l1 != NULL || l2 != NULL || carry != 0){
+            int val1 = (l1 != NULL) ? l1->val : 0;
+            int val2 = (l2 != NULL) ? l2->val : 0;
+
+            int sum = carry + val1 + val2;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            insertAtTail(ansHead, ansTail, digit);
+
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
+        }
+
+        return ansHead;
+    }
+
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        ListNode* ans = add(l1, l2);
+        ans=reverse(ans);
+
+        return ans;
+    }
+};
