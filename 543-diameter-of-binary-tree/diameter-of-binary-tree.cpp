114f68e45ae05d16df71bbc30c4984e08f9f8f35
Piyush Anand <contact.piyushanand@gmail.com>
2026-06-27 13:54:12 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (93.85%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,25 +11,20 @@
  */
 class Solution {
 public:
-
+    int ans=0;
     int height(TreeNode* root){
         if(root==NULL){
             return 0;
         }
         int left=height(root->left);
         int right=height(root->right);
+        ans=max(ans,left+right);
 
         return max(left,right)+1;
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        int left=diameterOfBinaryTree(root->left);
-        int right=diameterOfBinaryTree(root->right);
-        int curr=height(root->left)+height(root->right);
-
-        return max(curr, max(left,right));
+        height(root);
+        return ans;
     }
 };
