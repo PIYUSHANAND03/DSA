b45dc9240bfb5f9371b8609c848932963cf18763
Piyush Anand <contact.piyushanand@gmail.com>
2026-07-27 20:44:54 +0530

    Time: 2 ms (38.55%) | Memory: 63.1 MB (11.28%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,17 +4,16 @@ public:
         int max_water=0;
         int i=0, j=height.size()-1;
         while(i<j){
-                int width=j-i;
-                int min_height=min(height[i],height[j]);
-                int water=width*min_height;
-                max_water=max(max_water,water);
-
-                if(height[i]<height[j]){
-                    i++;
-                }
-                else{
-                    j--;
-                }
+            int width=j-i;
+            int min_height=min(height[i], height[j]);
+            int area=min_height*width;
+            max_water=max(max_water, area);
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
         return max_water;
     }
