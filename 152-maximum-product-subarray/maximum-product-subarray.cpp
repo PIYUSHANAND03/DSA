3f2008a40da120104a37eadcc229aef16ccacc22
Piyush Anand <contact.piyushanand@gmail.com>
2026-08-09 13:08:39 +0530

    Time: 359 ms (6.08%) | Memory: 17.8 MB (61.09%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxi=nums[0];
+        for(int i=0;i<nums.size();i++){
+            int product=1;
+            for(int j=i;j<nums.size();j++){
+                product=product*nums[j];
+                maxi=max(maxi,product);
+            }
+        }
+        return maxi;
+    }
+};
